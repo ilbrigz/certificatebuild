ff29034741b288be46811421b3ecbe5a6ee6a263
ilbrigz <ilogirb23@yahoo.com>
2020-02-10 11:57:21 +0800

    .

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ import pdfMake from 'pdfmake';
 import vfs from './vfs_fonts.js';
 import './App.css';
 import { readAndCompressImage } from 'browser-image-resizer';
-import cloneDeep from 'clone-deep'
-import jexcel from 'jexcel'
+import cloneDeep from 'clone-deep';
+import jexcel from 'jexcel';
 
-
-import Canvas from './components/Canvas'
+import Canvas from './components/Canvas';
 
 import {
   preventOutsideMovement,
@@ -16,61 +15,58 @@ import {
 } from './utilty/canvass_helper.js';
 
 import {
-  centeredTextProperties, leftOrRightAlignedTextProperties, textboxMargin
-} from './utilty/pdf_helper'
+  centeredTextProperties,
+  leftOrRightAlignedTextProperties,
+  textboxMargin,
+} from './utilty/pdf_helper';
 
-import { toDataURL } from './utilty/helper'
-
-
+import { toDataURL } from './utilty/helper';
 
 export default function App() {
   const canvasRef = useRef(null);
   const divRef = useRef(null);
   const jexcelRef = useRef(null);
-  const fabricRef = useRef(null)
-  const [fontSize, setFontSize] = useState(16)
-  const [count, setCount] = useState(16)
+  const fabricRef = useRef(null);
+  const [fontSize, setFontSize] = useState(16);
+  const [count, setCount] = useState(16);
 
   useEffect(() => {
-    jexcelRef.current = jexcel(
-      divRef.current, {
+    jexcelRef.current = jexcel(divRef.current, {
       minDimensions: [10, 20],
       defaultColWidth: 100,
       tableOverflow: true,
-      tableWidth: "842px",
-      tableHeight: "500px",
-      minDimensions: [2, 2],
-      onchange: ((instance, cell, x, y, value) => {
-        console.log(instance)
-        console.log(cell)
-        console.log(x)
-        console.log(y)
-        console.log(value)
-      })
+      tableWidth: '842px',
+      tableHeight: '500px',
+      minDimensions: [2, 5],
+      onchange: (instance, cell, x, y, value) => {
+        console.log(instance);
+        console.log(cell);
+        console.log(x);
+        console.log(y);
+        console.log(value);
+      },
     });
-  }, [])
+  }, []);
 
   useEffect(() => {
     fabricRef.current = new fabric.Canvas(canvasRef.current, {
       objectCaching: false,
+      preserveObjectStacking: true,
       fontSize: 20,
     });
     fabricRef.current.on('object:moving', preventOutsideMovement);
-    ;
-
-
     fabricRef.current.on('selection:created', (e) => {
       if (e.target.fontSize) {
-        setFontSize(parseInt(e.target.fontSize))
+        setFontSize(parseInt(e.target.fontSize));
       }
-    })
+    });
 
     fabricRef.current.on('object:scaling', preventOutsideScaling);
     fabricRef.current.setHeight(595);
     fabricRef.current.setWidth(842);
     fabricRef.current.renderAll();
 
-    console.log(fabric)
+    console.log(fabric);
 
     fabric.Object.prototype.set({
       transparentCorners: false,
@@ -108,16 +104,19 @@ export default function App() {
       top: 0,
       fontFamily: 'Roboto',
     });
-    var t1 = new fabric.Textbox('Lorem ipsum dibus repellat iusto Lorem ipsum dibus repellat iusto Lorem ipsum dibus repellat iusto Lorem ipsum dibus repellat iusto.', {
-      width: 200,
-      top: 400,
-      hasControls: true,
-      left: 200,
-      fontSize: 16,
-      padding: 0,
-      textAlign: 'center',
-      fontFamily: 'Roboto',
-    });
+    var t1 = new fabric.Textbox(
+      'Lorem ipsum dibus repellat iusto Lorem ipsum dibus repellat iusto Lorem ipsum dibus repellat iusto Lorem ipsum dibus repellat iusto.',
+      {
+        width: 200,
+        top: 400,
+        hasControls: true,
+        left: 200,
+        fontSize: 16,
+        padding: 0,
+        textAlign: 'center',
+        fontFamily: 'Roboto',
+      }
+    );
     t1.setControlsVisibility({
       mt: false,
       mb: false,
@@ -130,13 +129,17 @@ export default function App() {
       mtr: false,
     });
 
-    fabric.Image.fromURL('/certificate2.jpg', function (img) {
-      console.log(img)
-      const image = fabricRef.current.setBackgroundImage(img, fabricRef.current.renderAll.bind(fabricRef.current), {
-        scaleX: fabricRef.current.width / img.width,
-        scaleY: fabricRef.current.height / img.height
-      });
-      console.log(img)
+    fabric.Image.fromURL('/certificate2.jpg', function(img) {
+      console.log(img);
+      const image = fabricRef.current.setBackgroundImage(
+        img,
+        fabricRef.current.renderAll.bind(fabricRef.current),
+        {
+          scaleX: fabricRef.current.width / img.width,
+          scaleY: fabricRef.current.height / img.height,
+        }
+      );
+      console.log(img);
     });
 
     fabricRef.current.add(text3, text2, text, t1);
@@ -147,7 +150,11 @@ export default function App() {
   const quality = 1;
 
   const generatePdf = async () => {
-    const image = await toDataURL('http://localhost:3000/certificate2.jpg', { maxWidth: pageWidth, maxHeight: pageHeight, quality })
+    const image = await toDataURL('http://localhost:3000/certificate2.jpg', {
+      maxWidth: pageWidth,
+      maxHeight: pageHeight,
+      quality,
+    });
     const pageBackground = {
       image: image,
       width: pageWidth,
@@ -156,8 +163,12 @@ export default function App() {
     };
 
     const jsonCanvas = fabricRef.current.toObject();
-    const headers = jexcelRef.current.getHeaders().split(',')
-    const filteredData = jexcelRef.current.getData().filter(a => a.some(function (x) { return x }));
+    const headers = jexcelRef.current.getHeaders().split(',');
+    const filteredData = jexcelRef.current.getData().filter((a) =>
+      a.some(function(x) {
+        return x;
+      })
+    );
 
     const singlePageObjects = [];
     const dynamicObjects = [];
@@ -165,9 +176,7 @@ export default function App() {
     const { objects } = jsonCanvas;
 
     for (let i = 0; i < objects.length; i++) {
-      if (
-        objects[i].type === 'i-text'
-      ) {
+      if (objects[i].type === 'i-text') {
         singlePageObjects.push({
           text: objects[i].text,
           absolutePosition: {
@@ -177,11 +186,8 @@ export default function App() {
           fontSize: objects[i].fontSize,
           alignment: objects[i].textAlign,
         });
-
       }
-      if (
-        objects[i].type === 'image'
-      ) {
+      if (objects[i].type === 'image') {
         singlePageObjects.push({
           image: objects[i].src,
           absolutePosition: {
@@ -191,7 +197,6 @@ export default function App() {
           width: objects[i].width * objects[i].scaleX,
           height: objects[i].height * objects[i].scaleY,
         });
-
       }
 
       if (objects[i].type === 'textbox') {
@@ -203,47 +208,53 @@ export default function App() {
           },
           fontSize: objects[i].fontSize,
           alignment: objects[i].textAlign,
-          margin: textboxMargin(objects[i].left,
-            objects[i].width,
-            842),
+          margin: textboxMargin(objects[i].left, objects[i].width, 842),
         });
       }
 
       if (objects[i].type === 'text') {
         dynamicObjects.push({
           text: headers.indexOf(objects[i].text),
-          ...(objects[i].textAlign === 'center' ?
-            centeredTextProperties(objects[i], pageWidth) :
-            leftOrRightAlignedTextProperties(objects[i], pageWidth)),
+          ...(objects[i].textAlign === 'center'
+            ? centeredTextProperties(objects[i], pageWidth)
+            : leftOrRightAlignedTextProperties(objects[i], pageWidth)),
           fontSize: objects[i].fontSize,
-
         });
       }
     }
 
     let pagesContent = [];
-
-
-    for (let i = 0; i < filteredData.length; i++) {
+    console.log(!filteredData.length || !dynamicObjects.length);
+    if (!filteredData.length || !dynamicObjects.length) {
       pagesContent = [
-        ...pagesContent,
         pageBackground,
         {
-          stack: [
-            ...singlePageObjects,
-            ...(loopThroughItems(dynamicObjects, filteredData[i])),
-          ],
+          stack: [...singlePageObjects],
           unbreakable: true,
-          ...(i !== filteredData.length - 1 && { pageBreak: 'after' }),
         },
       ];
+    } else {
+      for (let i = 0; i < filteredData.length; i++) {
+        pagesContent = [
+          ...pagesContent,
+          pageBackground,
+          {
+            stack: [
+              ...singlePageObjects,
+              ...loopThroughItems(dynamicObjects, filteredData[i]),
+            ],
+            unbreakable: true,
+            ...(i !== filteredData.length - 1 && { pageBreak: 'after' }),
+          },
+        ];
+      }
     }
 
     const docDefinition = cloneDeep({
       pageOrientation: 'landscape',
       pageMargins: 0,
       content: pagesContent,
-    })
+    });
 
     pdfMake.createPdf(docDefinition, null, null, vfs).open();
 
@@ -252,18 +263,20 @@ export default function App() {
       for (let i = 0; i < dynamicObjects.length; i++) {
         arrayObj.push({
           ...dynamicObjects[i],
-          text: rowData[dynamicObjects[i].text]
-        })
+          text: rowData[dynamicObjects[i].text],
+        });
       }
-      console.log(arrayObj)
-      return arrayObj
+      console.log(arrayObj);
+      return arrayObj;
     }
   };
 
   const alignCenter = () => {
     const activeEl = fabricRef.current.getActiveObject();
     if (activeEl && activeEl.type) {
-      activeEl.set({ left: fabricRef.current.getWidth() / 2 - activeEl.width / 2 });
+      activeEl.set({
+        left: fabricRef.current.getWidth() / 2 - activeEl.width / 2,
+      });
       activeEl.setCoords();
       fabricRef.current.requestRenderAll();
     }
@@ -272,51 +285,50 @@ export default function App() {
   const sendForward = () => {
     const activeEl = fabricRef.current.getActiveObject();
     if (activeEl && activeEl.type) {
-      fabricRef.current.bringForward(activeEl)
+      fabricRef.current.bringForward(activeEl);
       fabricRef.current.requestRenderAll();
     }
-  }
+  };
 
   const sendBackward = () => {
     const activeEl = fabricRef.current.getActiveObject();
     if (activeEl && activeEl.type) {
-      fabricRef.current.sendBackwards(activeEl)
+      fabricRef.current.sendBackwards(activeEl);
       fabricRef.current.requestRenderAll();
     }
-  }
+  };
 
   const onSetFontSize = (e) => {
-    console.log(e.target.value)
+    console.log(e.target.value);
     const activeEl = fabricRef.current.getActiveObject();
     if (activeEl) {
-      activeEl.set('fontSize', e.target.value.toString())
+      activeEl.set('fontSize', e.target.value.toString());
       fabricRef.current.requestRenderAll();
     }
-  }
+  };
 
   const onAlignText = (propertyValue, e) => {
     const activeEl = fabricRef.current.getActiveObject();
     if (activeEl) {
-      activeEl.set('textAlign', propertyValue)
+      activeEl.set('textAlign', propertyValue);
       fabricRef.current.requestRenderAll();
     }
-  }
+  };
 
   const onRemove = (e) => {
     const activeEl = fabricRef.current.getActiveObject();
     if (activeEl) {
-      fabricRef.current.remove(activeEl)
+      fabricRef.current.remove(activeEl);
       // fabricRef.current.requestRenderAll();
     }
-  }
+  };
 
   const onImageUpload = async (e) => {
-
     const config = {
       quality: 0.4,
       maxWidth: canvasRef.current.width / 2,
       maxHeight: canvasRef.current.height / 2,
-      mimeType: e.target.files[0].type
+      mimeType: e.target.files[0].type,
     };
 
     if (!e.target.files[0]) return;
@@ -325,13 +337,11 @@ export default function App() {
 
     const readerobj = new FileReader();
 
-    readerobj.onload = function () {
-
+    readerobj.onload = function() {
       var imgElement = document.createElement('img');
       imgElement.src = readerobj.result;
 
-      imgElement.onload = function () {
-
+      imgElement.onload = function() {
         var imageinstance = new fabric.Image(imgElement, {
           angle: 0,
           opacity: 1,
@@ -346,14 +356,11 @@ export default function App() {
           /** canvas ist landscape **/
           imageinstance.scaleToWidth(canvasRef.current.width - 200);
           imageinstance.scaleToHeight(canvasRef.current.height - 200);
-
         } else {
           /** canvas ist portrait **/
           imageinstance.scaleToHeight(canvasRef.current.height - 200);
           imageinstance.scaleToWidth(canvasRef.current.width - 200);
-
         }
-
 
         // imageinstance.setControlsVisibility(HideControls);
         //imageinstance.cornerSize(40);
@@ -370,25 +377,26 @@ export default function App() {
   const logCanvas = () => {
     const activeEl = fabricRef.current.getActiveObject();
     if (activeEl) {
-      console.log(activeEl.toObject())
-      return
+      console.log(activeEl.toObject());
+      return;
     }
     console.log(fabricRef.current.toObject());
     // console.log(JSON.stringify( fabricRef.current));
   };
 
   const stateChange = () => {
-    setCount(count + 1)
-  }
+    setCount(count + 1);
+  };
 
   return (
-    <div>
+    <div style={{ display: 'flex' }}>
       <div>
-        <p>{count}</p>
         <Canvas ref={canvasRef} />
         <div>
           <button onClick={() => onAlignText('left')}>Align left Text</button>
-          <button onClick={() => onAlignText('center')}>align Center Text</button>
+          <button onClick={() => onAlignText('center')}>
+            align Center Text
+          </button>
           <button onClick={() => onAlignText('right')}>Align right text</button>
         </div>
         <div>
@@ -401,17 +409,33 @@ export default function App() {
           <button onClick={onRemove}>X</button>
         </div>
 
-        <input type="file" id="file" onChange={onImageUpload} accept="image/*" />
-        <input type="number" name="quantity" defaultValue={fontSize} key={fontSize} min="10" max="80" onChange={onSetFontSize} />
+        <input
+          type="file"
+          id="file"
+          onChange={onImageUpload}
+          accept="image/*"
+        />
+        <input
+          type="number"
+          name="quantity"
+          defaultValue={fontSize}
+          key={fontSize}
+          min="10"
+          max="80"
+          onChange={onSetFontSize}
+        />
       </div>
       <div>
         <div ref={divRef}></div>
-        <button onClick={() => {
-          console.log(jexcelRef.current.getHeaders().split(','))
-          console.log(jexcelRef.current.getData())
-        }}>log data</button>
+        <button
+          onClick={() => {
+            console.log(jexcelRef.current.getHeaders().split(','));
+            console.log(jexcelRef.current.getData());
+          }}
+        >
+          log data
+        </button>
       </div>
     </div>
   );
-
 }
--- src/utilty/helper.js
+++ src/utilty/helper.js
@@ -25,13 +25,29 @@ export const getBase64ImageFromURL = (url) => {
   });
 };
 
-export const toDataURL = (url, config) => fetch(url)
-  .then(response => response.blob())
-  .then(blob => readAndCompressImage(blob, config)).then((blob) => new Promise((resolve, reject) => {
-    const reader = new FileReader()
-    reader.onloadend = () => resolve(reader.result)
-    reader.onerror = reject
-    reader.readAsDataURL(blob)
-  }))
+// export const toDataURL = (url, config) =>
+//   fetch(url)
+//     .then((response) => response.blob())
+//     .then((blob) => readAndCompressImage(blob, config))
+//     .then(
+//       (blob) =>
+//         new Promise((resolve, reject) => {
+//           const reader = new FileReader();
+//           reader.onloadend = () => resolve(reader.result);
+//           reader.onerror = reject;
+//           reader.readAsDataURL(blob);
+//         })
+//     );
 
-
+export const toDataURL = (url, config) =>
+  fetch(url)
+    .then((response) => response.blob())
+    .then(
+      (blob) =>
+        new Promise((resolve, reject) => {
+          const reader = new FileReader();
+          reader.onloadend = () => resolve(reader.result);
+          reader.onerror = reject;
+          reader.readAsDataURL(blob);
+        })
+    );
