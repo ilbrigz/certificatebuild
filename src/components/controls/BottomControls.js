b874da5fe6a95dc4a081fc05b916248096499f91
ilbrigz <ilogirb23@yahoo.com>
2020-03-03 15:41:24 +0800

    some styling added

--- src/components/controls/BottomControls.js
+++ src/components/controls/BottomControls.js
@@ -8,61 +8,56 @@ import useControlHanders from './useControlHanders';
 import generatePdf from '../../modules/pdfmake.module';
 
 const BottomControls = () => {
-    const { selectedObject, fabricRef, jexcelRef } = React.useContext(AppContext);
-    const {
-        insertText,
-        onImageUpload,
-        logCanvas,
-        testing,
-    } = useControlHanders();
+  const { selectedObject, fabricRef, jexcelRef } = React.useContext(AppContext);
+  const { insertText, onImageUpload, logCanvas, testing } = useControlHanders();
 
-    const onGeneratePdf = () => {
-        generatePdf({ fabricRef, jexcelRef });
-    };
-    return (
-        <>
-            <label className="myLabel">
-                <input
-                    type="file"
-                    id="file"
-                    onChange={onImageUpload}
-                    accept="image/*"
-                />
-                <span>Upload Image</span>
-            </label>
+  const onGeneratePdf = () => {
+    generatePdf({ fabricRef, jexcelRef });
+  };
+  return (
+    <>
+      <label className="myLabel">
+        <input
+          type="file"
+          id="file"
+          onChange={onImageUpload}
+          accept="image/*"
+        />
+        <span>Upload Image</span>
+      </label>
 
-            <Button variant="outlined" data-tip data-for="clickme">
-                Insert Editable Text
+      <Button variant="outlined" data-tip data-for="clickme">
+        Insert Editable Text
       </Button>
-            <ReactTooltip
-                id="clickme"
-                place="bottom"
-                delayHide={1000}
-                effect="solid"
-                clickable={true}
-            >
-                <Button variant="outlined" onClick={insertText}>
-                    TEXT
+      <ReactTooltip
+        id="clickme"
+        // place="bottom"
+        delayHide={1000}
+        effect="solid"
+        clickable={true}
+      >
+        <Button variant="outlined" onClick={insertText}>
+          TEXT
         </Button>
-                <Button variant="outlined" onClick={insertText}>
-                    TEXTBOX
+        <Button variant="outlined" onClick={insertText}>
+          TEXTBOX
         </Button>
-            </ReactTooltip>
+      </ReactTooltip>
 
-            <Button variant="outlined" variant="outlined">
-                test
-        </Button>
-            <Button variant="outlined" onClick={logCanvas}>
-                LOG JSON
-        </Button>
-            <Button variant="outlined" onClick={onGeneratePdf}>
-                download
-        </Button>
-            <Button variant="outlined" onClick={testing}>
-                testing
-        </Button>
-        </>
-    );
+      <Button variant="outlined" variant="outlined">
+        test
+      </Button>
+      <Button variant="outlined" onClick={logCanvas}>
+        LOG JSON
+      </Button>
+      <Button variant="outlined" onClick={onGeneratePdf}>
+        download
+      </Button>
+      <Button variant="outlined" onClick={testing}>
+        testing
+      </Button>
+    </>
+  );
 };
 
 export default BottomControls;
