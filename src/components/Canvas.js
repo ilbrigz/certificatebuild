39e05219a3d3209c7aa1e407b1699f15040fb8fd
ilbrigz <ilogirb23@yahoo.com>
2020-03-16 09:26:58 +0800

    .

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -2,31 +2,77 @@ import React from 'react';
 import { fabric } from 'fabric';
 import { AppContext } from '../context';
 import FontFaceObserver from 'fontfaceobserver';
-import { Paper } from '@material-ui/core';
+import { Paper, makeStyles } from '@material-ui/core';
+import { useGesture, useWheel } from 'react-use-gesture'
+import { useSpring, animated } from 'react-spring'
 
 import { fabricOptionsOveride } from '../config/fabric.config';
 import data from '../data/fabric';
 import { addUndoRedo, addFabricKeyListener } from '../modules/fabric.module';
 
 import { preventOutsideMovement } from '../utilty/canvass_helper.js';
-console.log(data);
+
+
+
+
+
+const useStyles = makeStyles((theme) => ({
+  canvasContainer: {
+    backgroundColor: 'red',
+    overflow: 'hidden',
+    maxWidth: '100%',
+    maxHeight: '100%',
+    position: 'relative',
+    '&>div': {
+      position: 'relative',
+      width: 'auto',
+      height: 'auto',
+      transition: "all .2s ease-in-out",
+    },
+  },
+}));
 const Canvas = () => {
+  const outer = useWheel(state => console.log(state), { eventOptions: { captureEvents: false } })
+  const [scale, setScale] = React.useState(1)
+  const [translateX, setTranslateX] = React.useState(0)
+  const [translateY, setTranslateY] = React.useState(0)
+  const containerRef = React.useRef(null);
+  const canvasRef = React.useRef(null);
+  const convasInnerContainer = React.useRef(null);
+  const bind = useWheel(
+    ({ event }) => {
+      console.log('from inner')
+      event.preventDefault()
+      event.stopPropagation()
+    }, { domTarget: convasInnerContainer, eventOptions: { passive: false } }
+  )
+  const classes = useStyles()
   const windowSizeRef = React.useRef();
   const { fabricRef, setSelectedObject, selectedObject } = React.useContext(
     AppContext
   );
-  const containerRef = React.useRef(null);
-  const canvasRef = React.useRef(null);
+
+
+
+  const [props, set, stop] = useSpring(() => ({ transform: `scale(${scale}) translateX(${translateX}px) translateY(${translateY}px)`, config: { duration: 100 } }))
+  set({ transform: `scale(${scale}) translateX(${translateX}px) translateY(${translateY}px)` })
+
+  React.useEffect(bind, [bind])
+
 
   const handleUserKeyPress = React.useCallback((e) => {
     addFabricKeyListener(fabricRef, e);
   }, []);
 
   const updateObjectSize = React.useCallback((e) => {
+    //check the updated width
     var width = window.innerWidth
       || document.documentElement.clientWidth
       || document.body.clientWidth;
-      if(width === windowSizeRef.current){return}
+    //if the width are the same it means the resize is caused by the
+    //android keyboard
+    //do nothing
+    if (width === windowSizeRef.current) { return }
     const oldWidth = fabricRef.current.width;
     const containerDim = containerRef.current.getBoundingClientRect();
     fabricRef.current.setHeight(containerDim.width * (595 / 842));
@@ -47,9 +93,10 @@ const Canvas = () => {
       (backgroundImage.top / oldWidth) * fabricRef.current.width;
     fabricRef.current.discardActiveObject();
     fabricRef.current.renderAll();
-   windowSizeRef.current = window.innerWidth
-    || document.documentElement.clientWidth
-    || document.body.clientWidth
+    //update the windowSizeRef for future reference
+    windowSizeRef.current = window.innerWidth
+      || document.documentElement.clientWidth
+      || document.body.clientWidth
   }, []);
 
   React.useEffect(() => {
@@ -103,10 +150,7 @@ const Canvas = () => {
         (backgroundImage.top / 842) * fabricRef.current.width;
       fabricRef.current.discardActiveObject();
       fabricRef.current.renderAll();
-      // fabricRef.current.renderAll();
     });
-    // fabricRef.current.historyInit()
-
     //fabric events
     fabricRef.current.on('object:moving', preventOutsideMovement);
     fabricRef.current.on('selection:created', (e) => {
@@ -124,6 +168,16 @@ const Canvas = () => {
       setSelectedObject({});
       window.removeEventListener('keydown', handleUserKeyPress);
     });
+
+    canvas.on('mouse:down', (e) => {
+      if (e.target !== null) {
+        // e.stopPropagation();
+        //Ignore this event, we are clicking on an object (event.target is clicked object)
+        return;
+      }
+      // e.stopPropagation();
+      // e.preventDefault();
+    })
 
     fabricRef.current.on('object:moving', function (options) {
       if (
@@ -147,9 +201,40 @@ const Canvas = () => {
   }, []);
 
   return (
-    <Paper square elevation={3} ref={containerRef}>
-      <canvas ref={canvasRef} id="canvas"></canvas>
-    </Paper>
+    <>
+      <Paper square
+        {...outer()}
+        className={classes.canvasContainer} elevation={3} ref={containerRef}>
+        <animated.div style={props} ref={convasInnerContainer}>
+          <canvas ref={canvasRef} id="canvas"></canvas>
+        </animated.div>
+      </Paper>
+      <button onClick={() => {  // Update spring with new props
+        setScale(scale + .1)
+        // Stop animation
+        // stop()
+      }}>scale up</button>
+      <button onClick={() => {  // Update spring with new props
+        setScale(scale - .1)
+      }}>scale down</button>
+      <button onClick={() => {  // Update spring with new props
+        setTranslateX(translateX + 16)
+      }}>move left</button>
+      <button onClick={() => {  // Update spring with new props
+        setTranslateX(translateX - 16)
+      }}>move right</button>
+      <button onClick={() => {  // Update spring with new props
+        setTranslateY(translateY + 16)
+      }}>move up</button>
+      <button onClick={() => {  // Update spring with new props
+        setTranslateY(translateY - 16)
+      }}>move down</button>
+      <button onClick={() => {  // Update spring with new props
+        setTranslateY(0)
+        setTranslateX(0)
+        setScale(1)
+      }}>reset</button>
+    </>
   );
 };
 
