ce2c1f2ee2fd47f9b597e50abcebc174014dd259
unknown <ilogirb23@yahoo.com>
2023-09-12 10:26:21 +0800

    Updates

---
+++ precache-manifest.76748ac42a9243b1a9b47ad121e9009f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f08017fd526154797c2d4dcc78cbd80",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b4c2fcd34af609057ecb",
+    "url": "./static/css/2.47471943.chunk.css"
+  },
+  {
+    "revision": "3c6db0ee3de41d5253f1",
+    "url": "./static/css/main.61181ca6.chunk.css"
+  },
+  {
+    "revision": "b4c2fcd34af609057ecb",
+    "url": "./static/js/2.afd70d3f.chunk.js"
+  },
+  {
+    "revision": "35e4c4978dfff31911794ae5ebc22b2f",
+    "url": "./static/js/2.afd70d3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c6db0ee3de41d5253f1",
+    "url": "./static/js/main.3760a588.chunk.js"
+  },
+  {
+    "revision": "5d94a71cd346670345db",
+    "url": "./static/js/runtime-main.e1fe4cb8.js"
+  }
+]);
